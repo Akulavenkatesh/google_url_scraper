cc37908a054481468964c99e5f2ea4096184f8c0
Venkatesh <akulavenkatesh20@gmail.com>
2017-07-14 09:45:00 +0530

    * Fixed the keywords list

--- script.py
+++ script.py
@@ -1,13 +1,7 @@
 from google import search
-from random import randint
-from time import sleep
-import csv
 
 
-keywords = ['Vijay Singh Pro Golfer',
-'William McGirt	Pro Golfer'
-'Rahul Dravid Cricket'
-'Sachin Tendulkar Cricket']
+keywords = ['Vijay Singh Pro Golfer', 'William McGirt	Pro Golfer', 'Rahul Dravid Cricket', 'Sachin Tendulkar Cricket']
 
 for words in keywords:
     a = '\n''%s '%words +"site:en.wikipedia.org/wiki/" '\n'
